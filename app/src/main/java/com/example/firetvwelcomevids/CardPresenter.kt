f9acdc17f5ad5902f4be7e94504552e7838c7c7a
FelipeJuarezJr <mr.felipe.juarez.jr@gmail.com>
2023-05-11 15:45:02 -0600

    new edits

--- app/src/main/java/com/example/firetvwelcomevids/CardPresenter.kt
+++ app/src/main/java/com/example/firetvwelcomevids/CardPresenter.kt
@@ -14,7 +14,8 @@ import kotlin.properties.Delegates
  * A CardPresenter is used to generate Views and bind Objects to them on demand.
  * It contains an ImageCardView.
  */
-class CardPresenter : Presenter() {
+class
+CardPresenter : Presenter() {
     private var mDefaultCardImage: Drawable? = null
     private var sSelectedBackgroundColor: Int by Delegates.notNull()
     private var sDefaultBackgroundColor: Int by Delegates.notNull()
